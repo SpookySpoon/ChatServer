121a97571dffca8ae41f99f57f39996452ce5530
SpookySpoon <xoxlovval@gmail.com>
2016-12-02 09:13:51 +0300

    hhh

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "tcpconnection.h"
 int main(int argc, char *argv[])
 {
-    //SomeChanges
+    //SomeChanges obana
     QCoreApplication a(argc, argv);
     qDebug()<<"Main thread is "<<QThread::currentThreadId();
     TcpMsgServer opa;
